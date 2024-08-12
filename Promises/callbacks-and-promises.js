93a3917db9a5ac8fed2a66d33561fa77ee8b962d
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-08-12 17:07:52 +0200

    Using promise instead of callback

--- Promises/callbacks-and-promises.js
+++ Promises/callbacks-and-promises.js
@@ -2,29 +2,64 @@
 const userLeft = false;
 const userWatchingCatMeme = false;
 
-function watchTutorialCallBack(callback,errorCallback)
+// Callback
+// function watchTutorialCallBack(callback,errorCallback)
+// {
+//     if(userLeft)
+//     {
+//         errorCallback({
+//             name: 'User left',
+//             message: ':('
+//         })
+//     }
+//     else if(userWatchingCatMeme)
+//     {
+//         errorCallback({
+//             name: 'User watching cat meme',
+//             message: 'WebDevSimplified < Cat'
+//         })
+//     } else
+//     {
+//         callback ('Thumbs up and Subs')
+//     }
+// }
+
+// watchTutorialCallback((message) => {
+//     console.log('Success: ' + message)
+// }, (error) => {
+//     console.log(error.name + ' ' + error.message)
+// })
+
+
+
+// Promise
+function watchTutorialPromise()
 {
-    if(userLeft)
-    {
-        errorCallback({
-            name: 'User left',
-            message: ':('
-        })
-    }
-    else if(userWatchingCatMeme)
-    {
-        errorCallback({
-            name: 'User watching cat meme',
-            message: 'WebDevSimplified < Cat'
-        })
-    } else
-    {
-        callback ('Thumbs up and Subs')
-    }
+    return new Promise((resolve,reject) => {
+        if(userLeft)
+            {
+                reject({
+                    name: 'User left',
+                    message: ':('
+                })
+            }
+            else if(userWatchingCatMeme)
+            {
+                reject({
+                    name: 'User watching cat meme',
+                    message: 'WebDevSimplified < Cat'
+                })
+            } else
+            {
+                resolve ('Thumbs up and Subs')
+            }
+    })
 }
 
-watchTutorialCallBack((message) => {
-    console.log('Success: ' + message)
-}, (error) => {
-    console.log(error.name + ' ' + error.message)
-})
+watchTutorialPromise()
+    .then((message) => {
+        console.log('Success: ' + message)
+    })
+    .catch((error) => {
+        console.log(error.name + ' ' + error.message)
+    })
